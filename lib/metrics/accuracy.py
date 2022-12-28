ff7a514e661b9ea5595eb300faa7cccb7ddded0b
RobbeRDG <robbedegroeve@gmail.com>
2022-12-28 19:31:41 +0100

    Gave every ex session it's own lib

--- lib/metrics/accuracy.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def get_accuracy(clf, x, y):
-    y_pred = clf(x)
-    return torch.div(
-        (y_pred == y).sum(),
-        len(y)
-    )
--- lib/models/hlim.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import nn
-
-
-class HardLimit(nn.Module):
-    values = torch.tensor([0.])
-
-    def forward(self, x):
-        return torch.heaviside(x, self.values)
